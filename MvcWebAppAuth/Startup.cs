3741e689f2e08baa2cd52a0ba3a9c18124bc0d7c
Uday <uphugar@DPC>
2019-08-19 16:01:11 +0530

    Add project files.

---
+++ MvcWebAppAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcWebAppAuth.Startup))]
+namespace MvcWebAppAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
